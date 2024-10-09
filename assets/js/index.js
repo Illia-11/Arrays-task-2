19cb1f15f0bea2026e4100ea2f5564d8323b6a33
Illia Piddubnyi <illya2008zp@gmail.com>
2024-10-09 10:51:24 +0300

    Add solution with arrow function

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,18 +3,37 @@
 // task 1
 console.log('===Task 1===');
 const getOddIndexElements = ['test', false, 12345, undefined, 'secret', NaN,true, 0, 100, null ];
-getOddIndexElements.forEach(function(name, i) {
-  if(i % 2 !== 0) {
+
+// getOddIndexElements.forEach(function(name, i) {
+//   if(i % 2 !== 0) {
+//     console.log(name);
+//   }
+// });
+
+
+console.log('===Task 1.2===');
+getOddIndexElements.forEach ((name, i) => {
+  if (i % 2 !== 0) {
     console.log(name);
   }
 });
 
-// task 2
+
+
+// // task 2
 console.log('===Task 2===');
 const getSentenceWithIndexAndValue = ['test', false, 2345, null];
-getSentenceWithIndexAndValue.forEach(function(name, i) {
-  console.log('Item with index ' + i + ' has value ' + name);
+// getSentenceWithIndexAndValue.forEach(function(name, i) {
+//   console.log('Item with index ' + i + ' has value ' + name);
+// });
+
+
+console.log('===Task 2.2===');
+getSentenceWithIndexAndValue.forEach ((name, i) => {
+  console.log(`Item with index ${i} has value ${name}`);
 });
+
+
 
 // task 3
 console.log('===Task 3===');
@@ -26,21 +45,41 @@ const people = [
   { firstName: 'Undefined', lastName: 'Undefined', age: 99 },
   { firstName: 'Null', lastName: 'Nullochyk', gender: 'male' }
 ];
-const getAdultPeople = people.filter(function (people) {
-  return people.age >= 18;
-});
+
+// const getAdultPeople = people.filter(function (people) {
+//   return people.age >= 18;
+// });
+// console.log(getAdultPeople);
+
+
+console.log('===Task 3.2===');
+const getAdultPeople = people.filter((people) => people.age >= 18);
 console.log(getAdultPeople);
+
+
 
 // task 4
 console.log('===Task 4===');
-const getFemalePerson = people.findIndex(function (people) {
-  return people.gender === 'female';
-});
+// const getFemalePerson = people.findIndex(function (people) {
+//   return people.gender === 'female';
+// });
+// console.log(getFemalePerson);
+
+
+console.log('===Task 4.2===');
+const getFemalePerson = people.findIndex ((people) => people.gender === 'female');
 console.log(getFemalePerson);
+
+
 
 // task 5
 console.log('===Task 5===');
-const getAdultMen = people.every(function(people) {
-  return people.age >= 18 && people.gender === 'male';
-});
+// const getAdultMen = people.every(function(people) {
+//   return people.age >= 18 && people.gender === 'male';
+// });
+// console.log(getAdultMen);
+
+
+console.log('===Task 5.2===');
+const getAdultMen = people.every((people) => people.age >= 18 && people.gender === 'male');
 console.log(getAdultMen);
